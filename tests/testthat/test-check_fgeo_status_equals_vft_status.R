d6260f64474db668f6c345185f1f1b0976f1ea50
Mauro Lepore <maurolepore@gmail.com>
2019-05-24 17:16:37 -0400

    WIP test that view$Status equals fgeo$DFstatus

--- tests/testthat/test-check_fgeo_status_equals_vft_status.R
+++ tests/testthat/test-check_fgeo_status_equals_vft_status.R
@@ -1,26 +1,49 @@
-test_that("check_fgeo_status_equals_vft_status", {
-  plotname <- "luquillo"
-  view <- read_view("FullTable", plotname)
-  fgeo <- tor::list_rdata(
-    here::here("data-raw", "private", "fgeo", plotname, "full")
+library(purrr)
+library(dplyr)
+
+test_that("`view$Status` equals `fgeo$DFstatus`", {
+  view <- fgeo.tool::read_vft(
+    test_path("private/View/luquillo/ViewFullTable_luquillo.csv")
   )
+  fgeo <- tor::list_rdata(test_path("private/fgeo/luquillo/full"))
   fgeo_combined <- purrr::reduce(fgeo, dplyr::bind_rows)
 
-  library(tidyverse)
-  add_id(fgeo_combined) %>%
-    filter(!is.na()) %>%
-    select(id, treeID, stemID) %>%
-    add_count(id) %>%
-    filter(n > 1)
+  n_startswith_na <- add_id(fgeo_combined) %>%
+    filter(grepl("^NA_", .data$id)) %>%
+    nrow()
+  expect_equal(n_startswith_na, 0L)
+
+  n_endswith_na <- add_id(fgeo_combined) %>%
+    filter(grepl("NA$", .data$id)) %>%
+    nrow()
+  expect_equal(n_endswith_na, 0L)
 
   f <- add_id(fgeo_combined)
-  length(f$id)
-  length(unique(f$id))
+  expect_equal(
+    length(f$id),
+    length(unique(f$id))
+  )
 
-
-
-  add_id(fgeo[[1]])
-  f <- add_id(fgeo[[1]])
   v <- add_id(view)
+  expect_equal(
+    length(v$id),
+    length(unique(v$id))
+  )
 
+  joint <- left_join(
+    select(v, id, Status),
+    select(add_id(fgeo_combined), id, DFstatus),
+    by = "id"
+  )
+
+  result <- joint %>%
+    mutate(
+      is_matching_status = purrr::map2_lgl(
+        .data$Status, .data$DFstatus, identical
+      )
+    ) %>%
+    filter(!is.na(Status)) %>%
+    filter(!is_matching_status)
+
+  expect_equal(nrow(result), 0L)
 })
